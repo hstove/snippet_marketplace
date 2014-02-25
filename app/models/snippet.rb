7669f87cd3095c0806984d4464540f45115bf5af
Hank Stoever <hstove@gmail.com>
2014-02-24 22:30:28 -0800

    code ray syntax highlighting

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -2,4 +2,13 @@ class Snippet < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :title, :code, :user_id, :language
   attr_accessible :title, :price, :code, :language, :description
+
+  def syntaxed_code
+    CodeRay.scan("\n#{code}", language).div(:css => :class).html_safe
+  end
+
+  def human_price
+    return "Free" if price == 0
+    "฿ #{price}"
+  end
 end
