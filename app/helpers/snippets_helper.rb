017fe7a84501af6a93ee7f8cee35d599671d49a1
Hank Stoever <hstove@gmail.com>
2014-02-24 23:27:01 -0800

    coinbase button helper

--- app/helpers/snippets_helper.rb
+++ app/helpers/snippets_helper.rb
@@ -1,2 +1,11 @@
 module SnippetsHelper
+  def coinbase_button snippet
+    coinbase = Rails.configuration.coinbase
+
+    money = snippet.price.to_money('BTC')
+    custom = "#{current_user.id}-#{snippet.id}"
+    button = coinbase.create_button snippet.title, money, snippet.description, custom
+
+    button.embed_html.html_safe
+  end
 end
