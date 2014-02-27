e81ea673517f94378ba6f193f71f0ae0639547b5
Hank Stoever <hstove@gmail.com>
2014-02-26 19:06:03 -0800

    adds spec_helper [cherry-pick]

---
+++ spec/spec_helper.rb
@@ -0,0 +1,60 @@
+require 'rubygems'
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
+# require 'mocha/setup'
+require 'capybara/rspec'
+require 'database_cleaner'
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+# Checks for pending migrations before tests are run.
+# If you are not using ActiveRecord, you can remove this line.
+ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+
+Capybara.javascript_driver = :webkit
+
+RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+  config.include Capybara::DSL
+  # config.include CapybaraMacros
+  # config.include MailerMacros
+  # config.include SpecMacros
+
+  # config.before{ Rails.configuration.queue.clear }
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+  config.after do
+    Rails.configuration.queue.clear
+  end
+
+  # If true, the base class of anonymous controllers will be inferred
+  # automatically. This will be the default behavior in future versions of
+  # rspec-rails.
+  config.infer_base_class_for_anonymous_controllers = false
+
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = "random"
+end
+
+
+
+
+
