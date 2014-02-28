a42c183b6e961e076b39e5efa3d971622e5bc576
Hank Stoever <hstove@gmail.com>
2014-02-28 00:12:47 -0800

    adds tests to order

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Order do
-  subject { build :order }
+  it { should validate_presence_of(:coinbase_id) }
+  it { should validate_presence_of(:coinbase_code) }
+  it { should validate_presence_of(:amount) }
+  it { should validate_presence_of(:usd_amount) }
+
+  it { should belong_to(:user) }
+  it { should belong_to(:snippet) }
 end
