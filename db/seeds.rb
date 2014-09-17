0b2d2e3cb213427abb50e81515fb551b281f8721
hank <hstove@gmail.com>
2014-09-17 11:19:13 -0700

    better seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,22 +16,22 @@ user = User.create!({
 user.snippets.create!({
   title: "FizzBuzz",
   code: %Q{
-  def fizz_buzz_1(max)
-    arr = []
-    (1..max).each do |n|
-      if ((n % 3 == 0) && (n % 5 == 0))
-        arr << "FizzBuzz"
-      elsif (n % 3 == 0)
-        arr << "Fizz"
-      elsif (n % 5 == 0)
-        arr << "Buzz"
-      else
-        arr << n
-      end
+def fizz_buzz_1(max)
+  arr = []
+  (1..max).each do |n|
+    if ((n % 3 == 0) && (n % 5 == 0))
+      arr << "FizzBuzz"
+    elsif (n % 3 == 0)
+      arr << "Fizz"
+    elsif (n % 5 == 0)
+      arr << "Buzz"
+    else
+      arr << n
     end
-    return arr
   end
+  return arr
+end
   },
   language: "Ruby",
-  price: 10
+  price: 0.01
 })
