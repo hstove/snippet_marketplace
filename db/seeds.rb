5a6f505dd769205fb0dda88c3773b0e5bcc661ab
hank <hstove@gmail.com>
2014-09-17 11:04:01 -0700

    seed the DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user = User.create!({
+  email: "admin@example.com",
+  password: "password",
+  password_confirmation: "password",
+  is_admin: true,
+})
+
+user.snippets.create!({
+  title: "FizzBuzz",
+  code: %Q{
+  def fizz_buzz_1(max)
+    arr = []
+    (1..max).each do |n|
+      if ((n % 3 == 0) && (n % 5 == 0))
+        arr << "FizzBuzz"
+      elsif (n % 3 == 0)
+        arr << "Fizz"
+      elsif (n % 5 == 0)
+        arr << "Buzz"
+      else
+        arr << n
+      end
+    end
+    return arr
+  end
+  },
+  language: "Ruby",
+  price: 10
+})
